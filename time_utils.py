3152eee313527f4c26a84b484387e70211a2ce8e
ramya72002 <nsriramya7@gmail.com>
2024-08-06 16:55:04 +0530

    title added

--- time_utils.py
+++
@@ -1,8 +0,0 @@
-from flask import jsonify
-from datetime import datetime
-
-def time_now():
-    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-def serve_time():
-    return jsonify({"time": time_now()})

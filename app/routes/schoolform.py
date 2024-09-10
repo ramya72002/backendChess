8c07f4b5d481e3514ed25f8428594bf54b841611
ramya72002 <nsriramya7@gmail.com>
2024-09-10 08:59:25 +0530

    hiring pipeline added

--- app/routes/schoolform.py
+++ app/routes/schoolform.py
@@ -1,6 +1,14 @@
 from flask import Blueprint, request, jsonify
 from pymongo import MongoClient
 from app.database import schoolform_coll
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+from flask import Blueprint, request, jsonify
+from app.utils.email_utils import send_email
+ 
+
 schoolform_bp = Blueprint('schoolform', __name__)
 
 @schoolform_bp.route('/submit_form', methods=['POST'])
@@ -66,3 +74,56 @@ def get_forms():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+@schoolform_bp.route('/send-email-form', methods=['POST'])
+def send_email_school_form():
+    data = request.json
+    email = data.get('email')
+
+    if not email :
+        return jsonify({"error": "Email, session link, date, time, and coach name are required"}), 400
+
+    try:
+        # Email configuration
+        sender_email = "connect@chesschamps.us"
+        sender_password = "iyln tkpp vlpo sjep"  # Use your app-specific password here
+        subject = "Welcome to the Chess After-School Program – Your Enrollment is Confirmed!"
+
+        body = (
+            f"Dear Participant,\n\n"
+            f"You have successfully enrolled in the Chess After-School Program.\n\n"
+            f"The program is designed to give students a fun and engaging way to learn the game while building critical thinking "
+            f"and problem-solving skills. Through interactive lessons and games, students will master key strategies, improve focus, "
+            f"and boost confidence, all in a supportive environment.\n\n"
+            
+            f"**Program Details:**\n"
+            f"- 10 Week Training [K-5 Students]\n"
+            f"- Duration: 25 Sep 2024 to 18 Dec 2024\n"
+            f"- Timing: 3:30 PM - 4:30 PM\n"
+            f"- Note: No classes on 27 Nov 2024\n\n"
+            
+            f"We hope you enjoy your chess sessions and look forward to helping you master this wonderful game!\n\n"
+            f"Best regards,\n"
+            f"connect@chesschamps.us"
+        )
+
+
+        # Create the email
+        msg = MIMEMultipart()
+        msg['From'] = sender_email
+        msg['To'] = email
+        msg['Subject'] = subject
+        msg.attach(MIMEText(body, 'plain'))
+
+        # Connect to the Gmail server and send the email
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender_email, sender_password)
+        text = msg.as_string()
+        server.sendmail(sender_email, email, text)
+        server.quit()
+
+        return jsonify({"message": "Email sent successfully"}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
